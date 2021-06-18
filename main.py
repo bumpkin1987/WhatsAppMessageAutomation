5369dc4366e87868adfef251e25c39d95ddc1ca3
Mat Boyles <bumpkin1987@gmail.com>
2021-06-18 18:08:05 -0400

    successfully implemented sending message to whatsApp

--- main.py
+++ main.py
@@ -5,27 +5,32 @@
 import datetime
 import pywhatkit
 
-def send_message(month_date, phone_number):
-    now = datetime.datetime.utcnow() - datetime.timedelta(hours=4)
-    current_time = now.strftime("%H:%M:%S")
+
+def send_message(date, group_identity):
+    # added a few minutes to allow processing the request
+    nowish = datetime.datetime.utcnow() - datetime.timedelta(hours=4) + datetime.timedelta(minutes=5)
+    current_time = nowish.strftime("%H:%M:%S")
 
     after_hours_start = '13:01:00'
     after_hours_end = '23:59:00'
 
-    whats_app_msg = f"Files for {month_date} have been sent to dropbox"
-    if current_time > after_hours_start and current_time < after_hours_end:
+    whats_app_msg = f"Files for {date} have been sent to dropbox"
+    if after_hours_start < current_time < after_hours_end:
         print('Sending WhatsApp message after hours of job completion')
-        pywhatkit.sendwhatmsg_instantly(phone_number, whats_app_msg)
-        # pywhatkit.sendwhatmsg_to_group(group_id: str, message: str, time_hour: int, time_min: int, wait_time: int = 20,
-        #                  print_wait_time: bool = True, tab_close: bool = False)
+        pywhatkit.sendwhatmsg_to_group(group_identity,
+                                       whats_app_msg, 
+                                       int(current_time[0:2]),
+                                       int(current_time[3:5]),
+                                       wait_time=20)
         print(current_time)
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print('hey')
+    group_id = 'DMezCEJbXxL8OGg8q4OljH'
     phone_number = "+16142707567"
     month_date = "06-10-2021"
-    send_message(month_date, phone_number)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    send_message(month_date, group_id)
+
